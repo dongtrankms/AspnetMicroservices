95495f9aa7b838e67ebba06cf388b444f9245460
dongtrankms <dongtran@kms-technology.com>
2023-12-09 18:04:58 +0700

    configure Discount.Grpc database

--- src/Services/Discount/Discount.gRPC/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.gRPC/Discount.Grpc/Program.cs
@@ -1,14 +1,29 @@
+using Discount.Grpc.Extensions;
+using Discount.Grpc.Repositories;
 using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    app.MigrateDatabase<Program>();
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
 
 app.Run();
