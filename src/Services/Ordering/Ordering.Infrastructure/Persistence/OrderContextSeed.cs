a8ba43c81df4999bcb46958d11f64d5e295acb42
dongtrankms <dongtran@kms-technology.com>
2023-12-13 12:20:21 +0700

    Ordering add Order checkout update delete features

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {UserName = "dong", FirstName = "Dong", LastName = "Tran", EmailAddress = "dong@gmail.com", AddressLine = "Tan Vien", Country = "Viet Nam", TotalPrice = 350 }
+            };
+        }
+    }
+}
